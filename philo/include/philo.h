cbebddb6656d5639dfe1a7d6c251c21a79990665
Alex Schenk <alexander.o.schenk@gmail.com>
2024-08-14 19:53:05 +0200

    initialize project structure

---
+++ philo/include/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aschenk <aschenk@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/14 19:21:56 by aschenk           #+#    #+#             */
+/*   Updated: 2024/08/14 19:29:42 by aschenk          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+TBD
+*/
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include "libft.h" // libft fcts, stdlib.h (malloc,..)
+
+# include <stdio.h> // perror, printf
+
+#endif
