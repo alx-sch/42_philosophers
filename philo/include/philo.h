ba2c9298c797e9550a641fb766ddcbe880b92253
Alexander Schenk <aschenk@c3a8c5.42berlin.de>
2024-09-03 15:34:53 +0200

    remove libft and add philo logo when compiling

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: aschenk <aschenk@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 19:21:56 by aschenk           #+#    #+#             */
-/*   Updated: 2024/08/14 19:29:42 by aschenk          ###   ########.fr       */
+/*   Updated: 2024/09/03 14:35:20 by aschenk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,11 @@ TBD
 #ifndef PHILO_H
 # define PHILO_H
 
-# include "libft.h" // libft fcts, stdlib.h (malloc,..)
-
-# include <stdio.h> // perror, printf
+# include <stdio.h> // perror(), printf()
+# include <string.h> // memset()
+# include <stdlib.h> // malloc(), free()
+# include <unistd.h> // write(), usleep()
+# include <sys/time.h> // gettimeofday()
+# include <pthread.h> // thread fcts, like pthread_create()
 
 #endif
